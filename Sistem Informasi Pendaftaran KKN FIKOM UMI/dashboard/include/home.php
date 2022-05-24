05ba59503871e7780b0441a8656d2a7d2945c0f1
muhfarhansyam <61403695+muhfarhansyam@users.noreply.github.com>
2022-05-24 09:29:36 +0800

    v.1.0

---
+++ Sistem Informasi Pendaftaran KKN FIKOM UMI/dashboard/include/home.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<head></head>
+<body>
+<h2>Selamat Datang, <?php $role == "Admin" ?  print($nama_admin) : print($nama); ?></h2>
+<center><h3><strong>Kuliah Kerja Nyata</strong></h3></center>
+<div class="row">
+    <div class="col-sm-12 col-md-8 col-lg-10 col-lg-offset-1">
+        <div class="card" style="margin-top: 50px">
+            <div class="card-header" data-background-color="blue">
+                <h4 class="title">Pengertian <?php echo $kelas['kelas']; ?></h4>
+            </div>
+            <div class="card-content">
+            <p class="justify">Kuliah Kerja Nyata (KKN) adalah bentuk kegiatan pengabdian kepada masyarakat oleh mahasiswa dengan pendekatan lintas keilmuan dan sektoral pada waktu dan daerah tertentu di Indonesia. 
+                Kuliah Kerja Nyata (KKN) adalah suatu bentuk pendidikan dengan cara memberikan pengalaman belajar kepada mahasiswa untuk hidup di tengah- tengah masyarakat di luar kampus dan secara langsung mengidentifikasi serta menangani masalah-masalah pembangunan yang dihadapi. KKN dilaksanakan  dalam upaya meningkatkan isi dan bobot pendidikan bagi mahasiswa dan untuk mendapatkan nilai tambah yang lebih besar pada perguruan tinggi. 
+                <p>  
+            </div> 
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-12 col-md-8 col-lg-10 col-lg-offset-1">
+        <div class="card" style="margin-top: 50px">
+            <div class="card-header" data-background-color="blue">
+                <h4 class="title">Latar Belakang <?php echo $kelas['kelas']; ?></h4>
+            </div>
+            <div class="card-content">
+            <p class="justify">Sebagai salah satu wadah belajar mahasiswa, kampus selalu memastikan mahasiswa dapat terwadahi dengan baik, baik secara akademik maupun organisasi. 
+            Dengan demikian, terciptanya manusia-manusia unggul dengan berbagai pengetahuan dan pengalaman untuk menjadi sumber daya yang dapat bersaing di dunia kerja dapat diwujudkan. 
+            Oleh sebab itu, perguruan tinggi islam swasta tertua, terbesar dan terkemuka di Kawasan Indonesia Timur, Universitas Muslim Indonesia (UMI) memberi ruang kepada mahasiswa 
+            untuk menimba ilmu dalam hal kesiapan kerja<p>
+            </div> 
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-sm-12 col-md-8 col-lg-10 col-lg-offset-1">
+        <div class="card" style="margin-top: 50px">
+            <div class="card-header" data-background-color="blue">
+                <h4 class="title">Tujuan KKN <?php echo $kelas['kelas']; ?></h4>
+            </div>
+            <div class="card-content">
+            <br>
+            1.Mendapatkan pengalaman untuk bekal setelah lulus, sekaligus melibatkan diri secara langsung belajar interaksi sosial, merumuskan masalah, menjadi problem solving terhadap lokasi tempat KKN berada dsb. 
+            </br>
+            <br>
+            2.Berbagi ilmu berdasarkan kemampuan jurusan yang selama ini sudah dipelajari, tentu saja ini bentuk dari kaderisasi pembangunan dari kawula muda untuk Indonesia.
+            </br>
+            <br>
+            3.Sebagai sarana untuk mentransformasikan ilmu diperguruan tinggi ke masyarakat di tempat mereka tinggali dan membantu masyarakat sekitar untuk memecahkan masalah secara komprehensif, lintas sektoral, pragmatis dsb
+            </br>
+            <br>
+            4.Untuk menambah ilmu pengetahuan tentang dunia kerja sehingga dapat digunakan ketika memasuki dunia kerja.
+            </br>
+            <br>
+            5.Untuk memberikan peluang atau kesempatan bagi mahasiswa untuk mengetahui dan memahami segala aktivitas yang dilakukan oleh masyarakat atau suatu instansi
+            </br>
+            </div> 
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-12 col-md-8 col-lg-10 col-lg-offset-1">
+        <div class="card" style="margin-top: 50px">
+            <div class="card-header" data-background-color="blue">
+                <h4 class="title">Dokumentasi <?php echo $kelas['kelas']; ?></h4>
+            </div>
+            <div class="card-content">
+        <center>    					
+        <br><img src="http://localhost/assets/img/foto1.JPEG" ></br>
+        <br><img src="http://localhost/assets/img/foto2.JPEG" ></br>
+        <br><img src="http://localhost/assets/img/foto3.JPEG" ></br>
+        <br><img src="http://localhost/assets/img/foto4.JPG" ></br>
+        </center>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
